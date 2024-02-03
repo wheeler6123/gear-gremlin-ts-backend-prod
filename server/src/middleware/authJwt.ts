42fae6b5cf73b897829b9b7f060ea404d9394952
John <wheeler6123@gmail.com>
2024-02-03 12:25:34 -0700

    commented out authJwt middleware file

--- server/src/middleware/authJwt.ts
+++ server/src/middleware/authJwt.ts
@@ -1,40 +1,40 @@
-import jwt, { TokenExpiredError } from 'jsonwebtoken';
-import db from '../models';
-import { NextFunction, Request, Response } from 'express';
-import { secret } from '../../config/Auth';
+// import jwt, { TokenExpiredError } from 'jsonwebtoken';
+// import db from '../models';
+// import { NextFunction, Request, Response } from 'express';
+// import { secret } from '../../config/Auth';
 
-interface RequestWithUserId extends Request {
-    userId?: string;
-}
+// interface RequestWithUserId extends Request {
+//     userId?: string;
+// }
 
-const User = db.user;
+// const User = db.user;
 
-const catchError = (err: any, res: Response) => {
-    if (err instanceof TokenExpiredError) {
-        return res.status(401).send({ message: 'Unauthorized! Access Token was expired!' });
-    }
+// const catchError = (err: any, res: Response) => {
+//     if (err instanceof TokenExpiredError) {
+//         return res.status(401).send({ message: 'Unauthorized! Access Token was expired!' });
+//     }
 
-    return res.sendStatus(401).send({ message: 'Unauthorized!' });
-}
+//     return res.sendStatus(401).send({ message: 'Unauthorized!' });
+// }
 
-const authJwt = {
-    verifyToken: (req: RequestWithUserId, res: Response, next: NextFunction) => {
-        let token = req.headers['x-access-token'];
+// const authJwt = {
+//     verifyToken: (req: RequestWithUserId, res: Response, next: NextFunction) => {
+//         let token = req.headers['x-access-token'];
 
-        if (!token) {
-            return res.status(403).send({ message: 'No token provided!' });
-        }
+//         if (!token) {
+//             return res.status(403).send({ message: 'No token provided!' });
+//         }
 
-        if(typeof token === 'string') {
-            jwt.verify(token, secret!, (err: any, decoded: any) => {
-                if (err) {
-                    return catchError(err, res);
-                }
-                req.userId = decoded.id;
-                next();
-            });
-        } else {
-            return res.status(403).send({ message: 'Failed to authenticate token' });
-        }
-    },
-}
+//         if(typeof token === 'string') {
+//             jwt.verify(token, secret!, (err: any, decoded: any) => {
+//                 if (err) {
+//                     return catchError(err, res);
+//                 }
+//                 req.userId = decoded.id;
+//                 next();
+//             });
+//         } else {
+//             return res.status(403).send({ message: 'Failed to authenticate token' });
+//         }
+//     },
+// }
