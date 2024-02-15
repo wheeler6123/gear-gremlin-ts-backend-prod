aa2229ff3dec8449d34dce840bf46e7413f5e86b
John <wheeler6123@gmail.com>
2024-02-15 13:53:53 -0700

    removed dist folder from previous build

--- server/dist/routes/CategoryTag.routes.js
+++
@@ -1,8 +0,0 @@
-import CategoryTagController from "../controllers/CategoryTag.controller.js";
-import { Router } from "express";
-const router = Router();
-export const categoryTagRoutes = (app) => {
-    app.use('/api/auth', router);
-    router.get('/categoryTags/user/:userId', CategoryTagController.getAllTagsByUser);
-    router.post('/categoryTags/user/:userId', CategoryTagController.createTag);
-};
--- server/dist/routes/UsageTag.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import UsageTagController from '../controllers/UsageTag.controller.js';
-const router = Router();
-export const usageTagRoutes = (app) => {
-    app.use('/api/auth', router);
-    router.get('/usageTags/user/:userId', UsageTagController.getAllTagsByUser);
-    router.post('/usageTags/user/:userId', UsageTagController.createTag);
-};
